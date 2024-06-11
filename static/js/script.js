3cbaac7e2e36cabab4c2c9908a08ef8a4ca12bd1
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-06-11 13:57:22 +0100

    FEAT(js): reworked product option selections

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+/* Function to sort json data by product flavours */
 function json_sort(json) {
     json.sort((a, b) => {
         const aValue = JSON.stringify(Object.values(a.fields.flavour).sort());
@@ -12,266 +13,205 @@ function json_sort(json) {
 }
 
 
-if (window.location.pathname === "/") {
-    $(document).ready(function () {
-        let newFlavour = document.getElementById("new-prod-flavours");
-        let sportsFlavour = document.getElementById("sports-prod-flavours");
-        let healthFlavour = document.getElementById("health-prod-flavours");
-        if (newFlavour) {
-            let newSelectedSize = $("#new-prod-sizes :selected").val().slice(0, -2);
-            for (let i = 0; i < json_new_prod.length; i++) {
-                let obj = json_new_prod[i]
-                if (newSelectedSize == obj.fields.size &&
-                    newFlavour.value != obj.fields.flavour) {
-                    newFlavour.options[i].disabled = true
-                } else if (newSelectedSize != obj.fields.size &&
-                    newFlavour.value != obj.fields.flavour) {
-                    newFlavour.options[i].disabled = true
-                }
-            }
-        }
-        if (sportsFlavour) {
-            let sportsSelectedSize = $("#sports-prod-sizes :selected").val().slice(0, -2);
-            for (let i = 0; i < json_sports_prod.length; i++) {
-                let obj = json_sports_prod[i]
-                if (sportsSelectedSize == obj.fields.size &&
-                    sportsFlavour.value != obj.fields.flavour) {
-                    sportsFlavour.options[i].disabled = true
-                } else if (sportsSelectedSize != obj.fields.size &&
-                    sportsFlavour.value != obj.fields.flavour) {
-                    sportsFlavour.options[i].disabled = true
-                }
-            }
-        }
-        if (healthFlavour) {
-            let healthSelectedSize = $("#health-prod-sizes :selected").val().slice(0, -2);
-            for (let i = 0; i < json_health_prod.length; i++) {
-                let obj = json_health_prod[i]
-                if (healthSelectedSize == obj.fields.size &&
-                    healthFlavour.value != obj.fields.flavour) {
-                    healthFlavour.options[i].disabled = true
-                } else if (healthSelectedSize != obj.fields.size &&
-                    healthFlavour.value != obj.fields.flavour) {
-                    healthFlavour.options[i].disabled = true
-                }
-            }
+/* Check for and hide duplicate flavours */
+function dupe_flavours() {
+    let dupeFlavours = {};
+    $("select > option").each(function () {
+        if (dupeFlavours[this.text]) {
+            $(this).hide();
+            $(this).addClass('hidden')
+        } else {
+            dupeFlavours[this.text] = this.value;
         }
     })
 }
 
 
-function featProductFlavour() {
-    let newFlavour = document.getElementById("new-prod-flavours");
-    let sportsFlavour = document.getElementById("sports-prod-flavours");
-    let healthFlavour = document.getElementById("health-prod-flavours");
-    if (newFlavour) {
-        let newSelectedSize = $("#new-prod-sizes :selected").val().slice(0, -2);
-        for (let i = 0; i < json_new_prod.length; i++) {
-            let obj = json_new_prod[i]
-            if (newSelectedSize == obj.fields.size) {
-                newFlavour.value = obj.fields.flavour
-                newFlavour.options[i].disabled = false;
-            } else if (newSelectedSize != obj.fields.size) {
-                newFlavour.options[i].disabled = true
-            }
-        }
-    }
-    if (sportsFlavour) {
-        let sportsSelectedSize = $("#sports-prod-sizes :selected").val().slice(0, -2);
-        for (let i = 0; i < json_sports_prod.length; i++) {
-            let obj = json_sports_prod[i]
-            if (sportsSelectedSize == obj.fields.size) {
-                sportsFlavour.value = obj.fields.flavour
-                sportsFlavour.options[i].disabled = false;
-            } else if (sportsSelectedSize != obj.fields.size) {
-                sportsFlavour.options[i].disabled = true
-            }
-        }
-    }
-    if (healthFlavour) {
-        let healthSelectedSize = $("#health-prod-sizes :selected").val().slice(0, -2);
-        for (let i = 0; i < json_health_prod.length; i++) {
-            let obj = json_health_prod[i]
-            if (healthSelectedSize == obj.fields.size) {
-                healthFlavour.value = obj.fields.flavour
-                healthFlavour.options[i].disabled = false;
-            } else if (healthSelectedSize != obj.fields.size) {
-                healthFlavour.options[i].disabled = true
-            }
-        }
-    }
-}
-
-
-function featProductSize() {
-    let prodFlavour = document.getElementById("prod-flavours");
-    let prodSize = document.getElementById("prod-sizes");
-    let selectedFlavour = $("#prod-flavours :selected").val()
-    for (let i = 0; i < json_prod.length; i++) {
-        let obj = json_prod[i]
-        if (selectedFlavour == obj.fields.flavour) {
-            prodSize.value = obj.fields.size;
-            prodFlavour.options[i].disabled = false
-        }
-        if (selectedFlavour != obj.fields.flavour) {
-            prodFlavour.options[i].disabled = true
-        }
-    }
-}
-
-
-function linkedProdDict() {
-    let products = {}
-    for (let i = 0; i < json_linked_prods.length; i++) {
-        if (json_linked_prods[i]) {
-            let obj = json_linked_prods[i]
-            let prod_id = obj.fields.product.toString()
-            if (products[prod_id] && !(products[prod_id])) {
-                products[prod_id] = {}
-                products[prod_id] = Object.assign(products[prod_id], {'prod_id': prod_id})
-                products[prod_id] = Object.assign(products[prod_id], {'flavours': []})
-
-            } else if (!(products[prod_id])) {
-                products[prod_id] = {}
-                products[prod_id] = Object.assign(products[prod_id], {'prod_id': prod_id})
-                products[prod_id] = Object.assign(products[prod_id], {'flavours': []})
-            }
-            if (products[prod_id]['flavours'] &&
-                !(obj.fields.flavour in products[prod_id]['flavours'])) {
-                products[prod_id]['flavours'].push(obj.fields.flavour)
-            } else {
-                products[prod_id]['flavours'] = [obj.fields.flavour]
-            }
-            if (products[prod_id]['sizes'] &&
-                !(obj.fields.size in products[prod_id]['sizes'])) {
-                products[prod_id]['sizes'].push(obj.fields.size)
-            } else {
-                products[prod_id]['sizes'] = [obj.fields.size]
-            }
-        }
-    }
-    return products
-}
-
-
-if (window.location.pathname.indexOf("/products/") !== -1) {
-    $(document).ready(function () {
-        let prodFlavour = document.getElementById("prod-flavours");
-        let dupeFlavours = {};
-        $("select > option").each(function () {
-            if(dupeFlavours[this.text]) {
-                $(this).hide();
-                $(this).addClass('hidden')
-            } else {
-                dupeFlavours[this.text] = this.value;
-            }
-        })
-
-        if (prodFlavour) {
-            let json_prod_sorted = json_sort(json_prod)
-            for (let i = 0; i < json_prod_sorted.length; i++) {
-                let obj = json_prod_sorted[i]
-                let selectedSize = $("#prod-sizes :selected").val()
-                if (selectedSize == obj.fields.size) {
-                    if (prodFlavour[i].value != obj.fields.flavour) {
-                        prodFlavour.options[i].disabled = true
-                        if (!($(prodFlavour[i]).hasClass('hidden'))) {
-                            prodFlavour.options[i].classList.add('oos')
-                        }
-                    }
-                }
-                else if (selectedSize != obj.fields.size) {
-                    prodFlavour.options[i].disabled = true
-                    if (!($(prodFlavour[i]).hasClass('hidden'))) {
-                        prodFlavour.options[i].classList.add('oos')
-                    }
-                }
-            }
-        }
-
-        let products = linkedProdDict()
-        for (let prod in products) {
-            let linkedProdFlavour = document.getElementById(products[prod]['prod_id'] + "-prod-flavours");
-            if (linkedProdFlavour) {
-                let linkedSelectedSize = $("#" + products[prod]['prod_id'] + "-prod-sizes :selected").val()
-                for (let opt = 0; opt < products[prod]['flavours'].length; opt++) {
-                    if (linkedProdFlavour && linkedProdFlavour[opt] && products[prod]['flavours']) {
-                        if (linkedSelectedSize == products[prod]['sizes'][opt] &&
-                            linkedProdFlavour.options[opt].value != products[prod]['flavours'][opt]) {
-                            linkedProdFlavour.options[opt].disabled = true;
-                            $(linkedProdFlavour.options[opt]).addClass('oos');
-                        } else if (linkedSelectedSize != products[prod]['sizes'][opt] &&
-                            linkedProdFlavour.options[opt].value != products[prod]['flavours'][opt]) {
-                            linkedProdFlavour.options[opt].disabled = true;
-                            $(linkedProdFlavour.options[opt]).addClass('oos');
-                        } else {
-                            linkedProdFlavour.value = products[prod]['flavours'][opt];
-                        }
-                    }
-                }
-            }
-        }
-    })
-}
-
-
+/* Function to check for duplicate flavours and hide the duplicates */
 function flavourMatch(prodFlavour, i) {
     let flavourCheck = prodFlavour.innerText.match(new RegExp(prodFlavour[i].value, 'g'))
     if (flavourCheck.length > 1) {
-        if (prodFlavour[i].disabled == true) {
+        if (prodFlavour[i].disabled === true) {
             prodFlavour.options[i].style.display = "none"
             prodFlavour.options[i].classList.add('hidden')
             prodFlavour.options[i].classList.remove('oos')
+            prodFlavour.options[i].removeAttribute('selected')
             prodFlavour[i].disabled = false
         }
     }
 }
 
 
-function productOptions() {
-    let prodFlavour = document.getElementById("prod-flavours");
-    if (prodFlavour) {
-        let selectedSize = $("#prod-sizes :selected").val()
-        for (let i = 0; i < json_prod.length; i++) {
-            let obj = json_prod[i]
-            if (selectedSize == obj.fields.size) {
-                if (prodFlavour[i].value != obj.fields.flavour) {
-                    prodFlavour.options[i].disabled = true
-                    prodFlavour.options[i].classList.add('oos')
-                } else {
-                    prodFlavour.options[i].disabled = false
-                    prodFlavour.options[i].classList.remove('hidden')
-                    prodFlavour.options[i].classList.remove('oos')
-                    prodFlavour.options[i].style.display = 'unset'
-                }
+/* Function to select the first eligible flavour if none is selected */
+function flavourSelect(flavour, json) {
+    if (flavour.selectedIndex === -1) {
+        for (let i = 0; i < json.length; i++) {
+            if (!($(flavour.options[i]).hasClass('hidden')) &&
+                !($(flavour.options[i]).hasClass('oos'))) {
+                flavour.selectedIndex = i
+                flavour.options[i].setAttribute('selected', true)
+                break
             }
-            else {
-                prodFlavour.options[i].disabled = true
-                prodFlavour.options[i].classList.add('oos')
-            }
-
-            flavourMatch(prodFlavour, i)
         }
     }
 }
 
 
-function linkedOptions() {
-    products = linkedProdDict()
-    for (let prod in products) {
-        let linkedProdFlavour = document.getElementById(products[prod]['prod_id'] + "-prod-flavours");
-        if (linkedProdFlavour) {
-            let linkedSelectedSize = $("#" + products[prod]['prod_id'] + "-prod-sizes :selected").val()
-            for (let opt in products[prod]['flavours']) {
-                if (linkedProdFlavour && linkedProdFlavour[opt] && products[prod]['flavours']) {
-                    linkedProdFlavour.options[opt].disabled = false;
-                    if (linkedSelectedSize == products[prod]['sizes'][opt] &&
-                        linkedProdFlavour.options[opt].value != products[prod]['flavours'][opt]) {
-                        linkedProdFlavour.options[opt].disabled = true;
-                        linkedProdFlavour.value = products[prod]['flavours'][opt];
+/* Function to check for out of stock flavours and disable the dropdown options */
+function oos_products(flavour,size,json) {
+    if (flavour) {
+        json_prod_sorted = json_sort(json)
+        for (let i = 0; i < json_prod_sorted.length; i++) {
+            let obj = json_prod_sorted[i]
+            if (flavour.options.selectedIndex !== -1) {
+                flavour.options[flavour.options.selectedIndex].setAttribute('selected', true)
+            }
+            if (Number(size) === obj.fields.size) {
+                if (flavour[i].value !== obj.fields.flavour) {
+                    flavour.options[i].disabled = true
+                    flavour.options[i].classList.add('oos')
+                    flavour.options[i].removeAttribute('selected')
+                    if (flavour.selectedIndex === i) {
+                        flavour.selectedIndex = -1
                     }
+                } else {
+                    flavour.options[i].disabled = false
+                    flavour.options[i].classList.remove('hidden')
+                    flavour.options[i].classList.remove('oos')
+                    flavour.options[i].style.display = 'unset'
+                    if (flavour.selectedIndex === -1) {
+                        flavour.options[i].setAttribute('selected', true)
+                    }
+                    else {
+                        flavour.options[flavour.selectedIndex].setAttribute('selected', true)
+                    }
+                    if (i !== flavour.selectedIndex) {
+                        flavour.options[i].removeAttribute('selected')
+                    }
+                }
+            }
+            else {
+                flavour.options[i].disabled = true
+                flavour.options[i].classList.add('oos')
+                flavour.options[i].removeAttribute('selected')
+                if (flavour.selectedIndex === i) {
+                    flavour.selectedIndex = -1
+                }
+            }
+
+            // Hide duplicate flavour options
+            flavourMatch(flavour, i)
+        }
+        // If no option is selected, select the first eligible option
+        flavourSelect(flavour, json_prod_sorted)
+    }
+}
+
+
+function linked_prod_options() {
+    // Get occurrences for products
+    let prod_count = {}
+    for (let i = 0; i < json_linked_prods.length; i++) {
+        if (json_linked_prods[i]) {
+            prod_id = json_linked_prods[i].fields.product
+            prod_count[prod_id] = (prod_count[prod_id] + 1) || 1
+        }
+    }
+
+    // For each product with <2 counts, delete them from the json
+    for (let prod = 0; prod < Object.entries(prod_count).length; prod++) {
+        if (Object.entries(prod_count)[prod][1] < 2) {
+            for (let i = 0; i < json_linked_prods.length; i++) {
+                if (json_linked_prods[i] !== undefined &&
+                    Number(json_linked_prods[i].fields.product) === Number(Object.entries(prod_count)[prod][0])) {
+                    delete json_linked_prods[i]
                 }
             }
         }
     }
+
+    // Update the json object to reset the indexes and object length
+    let js_i = 0;
+    for (let index in json_linked_prods) {
+        json_linked_prods[js_i] = json_linked_prods[index]
+        js_i++
+    }
+    json_linked_prods.length = js_i;
+
+    // Get the unique product IDs for linked products that have multiple flavours
+    let prod_ids = []
+    for (let entry in json_linked_prods) {
+        if (!(prod_ids.includes(json_linked_prods[entry].fields.product))) {
+            prod_ids.push(json_linked_prods[entry].fields.product)
+        }
+    }
+
+    // Disable/enable product flavours, based on stock, for linked products
+    for (let id in prod_ids) {
+        let linkedProdFlavour = document.getElementById(prod_ids[id] + "-prod-flavours");
+        let linkedSelectedSize = $("#" + prod_ids[id] + "-prod-sizes :selected").val()
+        // Disable/enable products in dropdowns according to stock
+        oos_products(linkedProdFlavour, linkedSelectedSize, json_linked_prods)
+    }
+}
+
+
+/* On homepage load, disable the product dropdown options for out of stock products */
+if (window.location.pathname === "/") {
+    $(document).ready(function () {
+        let newFlavour = document.getElementById("new-prod-flavours");
+        let newSelectedSize = $("#new-prod-sizes :selected").val().slice(0, -2);
+        let sportsFlavour = document.getElementById("sports-prod-flavours");
+        let sportsSelectedSize = $("#sports-prod-sizes :selected").val().slice(0, -2);
+        let healthFlavour = document.getElementById("health-prod-flavours");
+        let healthSelectedSize = $("#health-prod-sizes :selected").val().slice(0, -2);
+
+        oos_products(newFlavour,newSelectedSize,json_new_prod)
+        oos_products(sportsFlavour,sportsSelectedSize,json_sports_prod)
+        oos_products(healthFlavour,healthSelectedSize,json_health_prod)
+    })
+}
+
+
+/* When selecting other dropdown options on the homepage, check for stock and disable out of stock options */
+function featOptions() {
+    let newFlavour = document.getElementById("new-prod-flavours");
+    let newSelectedSize = $("#new-prod-sizes :selected").val().slice(0, -2);
+    let sportsFlavour = document.getElementById("sports-prod-flavours");
+    let sportsSelectedSize = $("#sports-prod-sizes :selected").val().slice(0, -2);
+    let healthFlavour = document.getElementById("health-prod-flavours");
+    let healthSelectedSize = $("#health-prod-sizes :selected").val().slice(0, -2);
+
+    oos_products(newFlavour,newSelectedSize,json_new_prod)
+    oos_products(sportsFlavour,sportsSelectedSize,json_sports_prod)
+    oos_products(healthFlavour,healthSelectedSize,json_health_prod)
+}
+
+
+/* On product page load, hide duplicate and out of stock flavours in dropdowns */
+if (window.location.pathname.indexOf("/products/") !== -1) {
+    $(document).ready(function () {
+        /* For the main product on the page */
+        let prodFlavour = document.getElementById("prod-flavours");
+        let selectedSize = $("#prod-sizes :selected").val()
+        dupe_flavours()
+        oos_products(prodFlavour,selectedSize,json_prod)
+
+        /* For linked products */
+        // linked_prod_options()
+    })
+}
+
+
+/* When changing product dropdown options, disable out of stock flavours */
+function productOptions() {
+    let prodFlavour = document.getElementById("prod-flavours");
+    let selectedSize = $("#prod-sizes :selected").val()
+    oos_products(prodFlavour,selectedSize,json_prod)
+}
+
+
+/* For linked products, when changing product dropdown options, disable out of stock flavours */
+function linkedOptions() {
+    linked_prod_options()
 }
