6c826955ab997902d27f1e9e9bbcaa54b5dbea47
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-06-26 18:54:50 +0100

    FEAT(admin): custom model ordering in admin panel

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,11 @@ from .models import *
 
 
 # Admin site models
-admin.site.register([Products,
-                     ProductDetails])
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    ordering = ['brand', 'product_name']
+
+
+@admin.register(ProductDetails)
+class ProductDetailsAdmin(admin.ModelAdmin):
+    ordering = ['product__brand', 'product__product_name']
