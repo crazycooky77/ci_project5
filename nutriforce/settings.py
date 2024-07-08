b53f20f49bec11c94f648d4b382b983fc40c2534
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-07-08 11:27:19 +0100

    FEAT(settings): cache save

--- nutriforce/settings.py
+++ nutriforce/settings.py
@@ -179,6 +179,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
     AWS_STORAGE_BUCKET_NAME = 'nutriforce'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
