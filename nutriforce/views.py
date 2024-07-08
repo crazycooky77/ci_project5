2cb0f5c6d564c8d17191113a8ffe1db2eb82c657
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-07-08 13:42:39 +0100

    FEAT(errors): adjustments for error 500

--- nutriforce/views.py
+++ nutriforce/views.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import render
@@ -8,8 +10,16 @@ def handler404(request, exception):
     return render(request, "404.html", status=404)
 
 
-def handler500(request, exception):
-    def _send_admin_500_email(e):
+def handler500(request):
+    type_, value, traceback = sys.exc_info()
+
+    def _send_admin_500_email(*args):
+        if args:
+            e = []
+            for arg in args:
+                e.append(arg)
+        else:
+            e = None
         admin_email = settings.CONTACT_EMAIL
         subject = render_to_string(
             'error_emails/admin_500_subject.txt')
@@ -23,5 +33,5 @@ def handler500(request, exception):
             settings.DEFAULT_FROM_EMAIL,
             [admin_email])
 
-    _send_admin_500_email(exception)
+    _send_admin_500_email(type_, value, traceback)
     return render(request, "500.html", status=500)
