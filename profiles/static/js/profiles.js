607f92f80284b86398a67afe20b0ed7ac9e9c899
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-07-09 17:51:14 +0100

    FEAT(style): mobile styling added for profile pages

--- profiles/static/js/profiles.js
+++ profiles/static/js/profiles.js
@@ -1,17 +1,9 @@
 function addrInputResize() {
     let selectBox = document.getElementById('id_country')
-    let inputBox = document.getElementById('id_county')
-    selectBox.style.width = (inputBox.getBoundingClientRect().width + 2) + 'px'
-}
+    let selectBoxWidth = document.getElementById('id_country').getBoundingClientRect().width
+    let inputBoxWidth = document.getElementById('id_county').getBoundingClientRect().width
 
-if (window.location.pathname === '/profile/add-address' ||
-    window.location.pathname.split("/")[2] === 'edit-address') {
-    $(document).ready(function () {
-        addrInputResize()
-        window.onresize = function() {
-            addrInputResize()
-        }
-    })
+    selectBox.style.width = (selectBoxWidth + (inputBoxWidth - selectBoxWidth)) + 'px'
 }
 
 
@@ -24,7 +16,23 @@ function mobileResize() {
 
     addrLine2.style.width = (addrLine1Width - (addrLine2Width - addrLine1Width)) + 'px'
     addrLine3.style.width = (addrLine1Width - (addrLine3Width - addrLine1Width)) + 'px'
+    addrInputResize()
 }
+
+
+if (window.innerWidth > 320) {
+    if (window.location.pathname === '/profile/add-address' ||
+        window.location.pathname.split("/")[2] === 'edit-address') {
+        $(document).ready(function () {
+            window.onload = function() {
+            addrInputResize()
+                }
+            window.onresize = function() {
+                addrInputResize()
+            }
+        })
+    }
+    }
 
 
 if (window.innerWidth <= 320) {
@@ -50,10 +58,55 @@ if (window.innerWidth <= 320) {
                 document.getElementsByClassName('label-input')[i].firstElementChild.textContent = ''
             }
             mobileResize()
-            addrInputResize()
             window.onresize = function () {
                 mobileResize()
-                addrInputResize()
+            }
+        }
+        if (window.location.pathname.includes('/profile/orders')) {
+            let headers = document.querySelectorAll('th')
+            let buttons = document.querySelector('table').querySelectorAll('button')
+            let prodImg = document.querySelectorAll('.order-prod-img')
+            for (let i = 0; i < headers.length; i++) {
+                if (headers[i].textContent === 'Date') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-calendar-day"></i>'
+                }
+                else if (headers[i].textContent === 'Order #') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-hashtag"></i>'
+                }
+                else if (headers[i].textContent === 'Tracking') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-truck-fast"></i>'
+                }
+                else if (headers[i].textContent === 'Order Status') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-spinner"></i>'
+                }
+                else if (headers[i].textContent === 'View') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-eye"></i>'
+                }
+                else if (headers[i].textContent === 'Product') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-bottle-water"></i>'
+                }
+                else if (headers[i].textContent === 'Size') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-weight-scale"></i>'
+                }
+                else if (headers[i].textContent === 'Quantity') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-arrow-up-9-1"></i>'
+                }
+                else if (headers[i].textContent === 'Individual Price') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-money-bill-1-wave"></i>'
+                }
+                else if (headers[i].textContent === 'Sum') {
+                    headers[i].innerHTML = '<i class="fa-solid fa-sack-dollar"></i>'
+                }
+            }
+
+            for (let j = 0; j < buttons.length; j++) {
+                if(buttons[j].textContent === 'View Order') {
+                    buttons[j].innerHTML = 'View'
+                }
+            }
+
+            for (let k = 0; k < prodImg.length; k++) {
+                prodImg[k].remove()
             }
         }
     })
