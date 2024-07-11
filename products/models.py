5d16676b366b756f0ff3ab1c4d2982ade205ea19
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-07-11 22:16:18 +0100

    FIX(models): minor change to default img

--- products/models.py
+++ products/models.py
@@ -14,7 +14,7 @@ class Products(models.Model):
     product_pic_url = models.URLField(max_length=1024,
                                       null=True,
                                       blank=True)
-    product_pic = models.ImageField(default='/media/default_img.jpeg',
+    product_pic = models.ImageField(default='media/default_img.jpeg',
                                     null=True,
                                     blank=True)
     description = models.TextField()
