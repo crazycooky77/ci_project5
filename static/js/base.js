c077c0dd0684242be44e390b20c9c506ff5a7871
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-07-17 22:41:53 +0100

    FEAT(script): updating JS for styling on resize

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,33 @@
 window.addEventListener("DOMContentLoaded", () => {
-    // Restrict the width of the hyperlink element on the logo to the logo image width
-    let logo = document.getElementsByClassName("nav-right")[0].firstElementChild
-    logo.style.width = window.getComputedStyle(logo.firstChild).getPropertyValue("width")
+    logoResize()
+    scrollOpt()
+    baseU500()
+    baseO500()
+})
 
+window.addEventListener("load", () => {
+    logoResize()
+    baseU500()
+    baseO500()
+})
+
+window.addEventListener("resize", () => {
+    logoResize()
+    baseU500()
+    baseO500()
+})
+
+
+function logoResize() {
+    // Restrict the width of the hyperlink element on the logo to the logo image width
+    let logo = document.getElementsByClassName("nav-right")[0]
+    let width = window.getComputedStyle(logo).getPropertyValue("width")
+    logo.querySelector('a').style.width = width
+    logo.querySelector('img').style.width = width
+}
+
+
+function scrollOpt() {
     let scrollBtn = document.getElementById("footer-top-link");
     window.onscroll = function () {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -11,7 +36,7 @@ window.addEventListener("DOMContentLoaded", () => {
             scrollBtn.style.visibility = "hidden";
         }
     }
-})
+}
 
 
 function topScroll() {
@@ -23,15 +48,72 @@ function topScroll() {
 function newSignup() {
     let newsModal = document.getElementById('news-modal')
     newsModal.style.display = 'block';
-
     let newsSpan = document.getElementById('news-modal').getElementsByClassName('modal-close')[0]
     newsSpan.onclick = function() {
         newsModal.style.display = 'none';
     }
-
     window.onclick = function(event) {
         if (!(newsModal.contains(event.target))) {
             newsModal.style.display = 'none';
+        }
+    }
+    window.onresize = function() {
+        newsModal.style.display = 'none';
+    }
+}
+
+
+function baseU500() {
+    if (window.innerWidth <= 800) {
+        let navIcons = document.getElementsByClassName('nav-left')[0]
+        navIcons.querySelector('button').lastChild.textContent = ''
+        let navAnchors = navIcons.querySelectorAll('a')
+        for (let i = 0; i < navAnchors.length; i++) {
+            navAnchors[i].lastChild.textContent = ''
+        }
+        let navButtons = document.getElementsByClassName('nav-buttons')[0].querySelectorAll('button')
+        for (let j = 0; j < navButtons.length; j++) {
+            if (navButtons[j].title === 'All Products') {
+                navButtons[j].innerHTML = 'All'
+            }
+            else if (navButtons[j].title === "New Products") {
+                navButtons[j].innerHTML = 'New'
+            }
+        }
+        let navPipes = document.getElementsByClassName('nav-buttons')[0]
+        navPipes.innerHTML = navPipes.innerHTML.replace(/\|/g, '')
+    }
+}
+
+
+function baseO500() {
+    if (window.innerWidth > 800) {
+        let navIcons = document.getElementsByClassName('nav-left')[0]
+        navIcons.querySelector('button').lastChild.textContent = ' ' + navIcons.querySelector('button').title
+        let navAnchors = navIcons.querySelectorAll('a')
+        for (let i = 0; i < navAnchors.length; i++) {
+            navAnchors[i].lastChild.textContent = ' ' + navAnchors[i].title
+        }
+        let navButtons = document.getElementsByClassName('nav-buttons')[0].querySelectorAll('button')
+        for (let j = 0; j < navButtons.length; j++) {
+            if (navButtons[j].title === 'All Products') {
+                navButtons[j].innerHTML = 'All Products'
+            }
+            else if (navButtons[j].title === "New Products") {
+                navButtons[j].innerHTML = "What's New"
+            }
+        }
+        let navPipes = document.getElementsByClassName('nav-buttons')[0].childNodes
+        for (let k = 1; k < navPipes.length - 1; k++) {
+            if ((navPipes[k].nodeName === 'BUTTON' &&
+                navPipes[k+1].nodeName === 'BUTTON')) {
+                navPipes[k].parentNode.insertBefore(document.createTextNode("|"), navPipes[k].nextSibling)
+            }
+            else if ((navPipes[k].nodeName === '#text' &&
+                navPipes[k-1].nodeName === 'BUTTON' &&
+                navPipes[k+1].nodeName === 'BUTTON')) {
+                navPipes[k].replaceWith(document.createTextNode("|"))
+            }
         }
     }
 }
