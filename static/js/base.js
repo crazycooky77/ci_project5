baa2fccc2bb1faf87f0c07175f8041466ec858f6
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-07-18 15:19:43 +0100

    FEAT(footer): adding JS and styling for footer positioning

--- static/js/base.js
+++ static/js/base.js
@@ -13,6 +13,7 @@ window.addEventListener("load", () => {
 
 window.addEventListener("resize", () => {
     logoResize()
+    footerResize()
     baseU500()
     baseO500()
 })
@@ -29,11 +30,45 @@ function logoResize() {
 
 function scrollOpt() {
     let scrollBtn = document.getElementById("footer-top-link");
+    let footLinks = document.getElementById("footer-links");
     window.onscroll = function () {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
             scrollBtn.style.visibility = "unset";
+            scrollBtn.style.width = "15%"
+            if (window.innerWidth <= 500) {
+                footLinks.style.width = "85%"
+            }
         } else {
             scrollBtn.style.visibility = "hidden";
+            if (window.innerWidth <= 500) {
+                scrollBtn.style.width = "0%"
+                footLinks.style.width = "100%"
+            }
+        }
+    }
+}
+
+
+function footerResize() {
+    let scrollBtn = document.getElementById("footer-top-link");
+    let footLinks = document.getElementById("footer-links");
+    if (scrollBtn.style.visibility === "hidden" || scrollBtn.style.visibility === "") {
+        if (window.innerWidth <= 500) {
+            scrollBtn.style.width = "0%"
+            footLinks.style.width = "100%"
+        }
+        else {
+            scrollBtn.style.width = "15%"
+            footLinks.style.width = "70%"
+        }
+    } else {
+        if (window.innerWidth <= 500) {
+            scrollBtn.style.width = "15%"
+            footLinks.style.width = "85%"
+        }
+        else {
+            scrollBtn.style.width = "15%"
+            footLinks.style.width = "70%"
         }
     }
 }
