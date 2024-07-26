a09217d4451daf713ccf6134fff819bd81363f43
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-07-26 16:34:35 +0100

    FIX(script): testing fix for safari iOS not scrolling to invalid input if focused

--- static/js/base.js
+++ static/js/base.js
@@ -42,6 +42,9 @@ function scrollOpt() {
     };
 }
 
+window.addEventListener('submit', function() {
+    $("input").blur();
+})
 
 // Function to style footer elements based on screen width and Back to top button visibility
 function footerResize() {
