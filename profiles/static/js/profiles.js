1822c86413c9f4dbc4aaeca48a7b4202deb636d5
Yasmin Escaros-Buechsel <yescaros@apple.com>
2024-07-28 11:00:32 +0100

    FEAT(script): better functionality for profile ham menu

--- profiles/static/js/profiles.js
+++ profiles/static/js/profiles.js
@@ -2,12 +2,19 @@
 if (window.location.pathname.includes('profile') ||
     window.location.pathname.includes('email') ||
     window.location.pathname === '/password/change/') {
+    let hamMenu = document.getElementById('ham-menu');
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 800) {
+            hamMenu.style.display = 'flex';
+        }
+        else {
+            hamMenu.style.display = 'none';
+        }
+    });
     document.body.addEventListener('click', function (e) {
         if (window.innerWidth <= 800) {
-            let hamMenu = document.getElementById('ham-menu');
             let hamIcon = document.getElementById('ham-icon-button');
-
-            if (hamIcon === e.target && (hamMenu.style.display === '' || hamMenu.style.display === 'none')) {
+            if ((hamIcon === e.target || hamIcon === e.target.parentElement) && (hamMenu.style.display === '' || hamMenu.style.display === 'none')) {
                 hamMenu.style.display = 'flex';
             } else if (hamMenu.style.display === 'flex' && !(hamMenu.contains(e.target))) {
                 hamMenu.style.display = 'none';
